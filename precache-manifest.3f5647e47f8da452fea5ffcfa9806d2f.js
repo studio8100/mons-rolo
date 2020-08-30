449fd04d71483d76ad7905bb231e034b3c19265f
Paul Carstens <studio8100@yahoo.com>
2020-08-30 11:27:40 -0400

    Updates

---
+++ precache-manifest.3f5647e47f8da452fea5ffcfa9806d2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96c529356a93b61f1e7577f536e476f8",
+    "url": "/mons-rolo/index.html"
+  },
+  {
+    "revision": "858da72bde1135c0c729",
+    "url": "/mons-rolo/static/css/main.e251f536.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/mons-rolo/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mons-rolo/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "858da72bde1135c0c729",
+    "url": "/mons-rolo/static/js/main.409b7933.chunk.js"
+  },
+  {
+    "revision": "6dea00ec4692232b6110",
+    "url": "/mons-rolo/static/js/runtime-main.037ccc31.js"
+  }
+]);
