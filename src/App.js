23af63f6b6963170a0e9d0ad0565e58146b7a24f
Paul Carstens <studio8100@yahoo.com>
2020-08-30 11:21:44 -0400

    Final Commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import { CardList } from './components/card-list/card-list.component';
 import './App.css';
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
@@ -17,12 +18,26 @@ class App extends Component {
     .then(response => response.json())
     .then(users => this.setState({ monsters: users }));
   }
+
+  handleChange = e => {
+    this.setState({ searchField: e.target.value})
+  };
   
   render() {
+
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+        monster.name.toLowerCase().includes(searchField.toLowerCase())
+        );
+
     return (
       <div className='App'>
-        <input type='search' placeholder='Search Minsters' onChange={e => console.log(e.target.value)}/>
-        <CardList monsters={this.state.monsters} />
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder='Search Monsters'
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
         
       </div>
     );
